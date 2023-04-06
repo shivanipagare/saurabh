9c977c5ad71fd6acd121952d516537c04e2227ce
shivanipagare <shivanipagare02@gmail.com>
2023-04-06 11:13:22 +0530

    this my firt commit

---
+++ jsclass1.js
@@ -0,0 +1,40 @@
+// //object
+// const person={
+//     key:"value",
+//     city:"bhopal",
+//     age:"21"
+// }
+// console.log(person.age)
+
+
+
+// var x=2
+// x+=5
+// console.log(x)
+
+
+
+// var x=5
+// var y=4
+// console.log(x>y)
+
+// var a= 10
+// var b= ++
+// console.log(b)
+
+// var age=18
+// var vote = (age>18)?"adult":"minor"
+// console.log(vote)
+
+// var person={
+//     name:"shivani"
+// }
+
+
+// var fruit=["mango"]
+// console.log(typeof(fruit))
+
+
+var a=4
+b=a++
+console.log(a)
